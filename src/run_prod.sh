4d945cc9857712aa3f66b114e53cb17ae3549049
muhydeen95 <alabimuhydeen@gmail.com>
2023-05-04 17:21:22 +0100

    init commit

--- src/run_prod.sh
+++ src/run_prod.sh
@@ -2,11 +2,11 @@ export GITCOMMIT=$(git rev-parse --short HEAD)
 export GITREF="$(git log -1 --pretty=format:"%D")"
 export GITTIMESTAMP="$(git log -1 --pretty=format:"%ai")"
 
-export ANNOUNCED_IP=127.0.0.1
-export SITE_HOST=yourdomain.com
-export FRONTEND_DNS_OR_IP=www.yourdomain.com
-export STRIVE_TOKEN_SECRET=fill_in_random_token
-export STRIVE_API_KEY=fill_in_random_token
+export ANNOUNCED_IP=54.159.14.157
+export SITE_HOST=goserp.co.uk
+export FRONTEND_DNS_OR_IP=www.goserp.co.uk
+export STRIVE_TOKEN_SECRET=57ca7f82bf274a3a97f45316f366ad0d
+export STRIVE_API_KEY=a57e504a1ac0410281a28bc38e8e1261
 
 export MEDIASOUP_MIN_PORT=40000 # Minimun RTC port for ICE, DTLS, RTP, etc.
 export MEDIASOUP_MAX_PORT=49999 # Maximum RTC port for ICE, DTLS, RTP, etc.
